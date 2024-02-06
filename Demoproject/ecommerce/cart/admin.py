a27fd708c1179c18ed996748dc794198bd8317ef
Thomas Mathew <thomasmathew1404@gmail.com>
2024-02-06 09:35:13 +0530

    added1

--- Demoproject/ecommerce/cart/admin.py
+++ Demoproject/ecommerce/cart/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from cart.models import Cart
 from cart.models import Order
 from cart.models import Account
+from django.http import HttpResponse
 # Register your models here.
 admin.site.register(Cart)
 admin.site.register(Order)
